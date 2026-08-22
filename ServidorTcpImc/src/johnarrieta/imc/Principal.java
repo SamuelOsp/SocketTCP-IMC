1652a9bfb6675ec7c064a15ac8ddc1ec22f46359
SamuelOsp <avilaospina1@gmail.com>
2026-08-21 20:05:47 -0500

    feat(servidor): implementar logica de calculo IMC, servidor TCP y subproceso cliente

---
+++ ServidorTcpImc/src/johnarrieta/imc/Principal.java
@@ -0,0 +1,15 @@
+package johnarrieta.imc;
+
+import johnarrieta.imc.vistas.VentanaPrincipal;
+
+/**
+ * @author Samuel David Ospina De Avila
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+}
